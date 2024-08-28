4fedfd204a7576c4f0f7004a3141937ddd77597f
Vivek Kumar <118689020+vickvey@users.noreply.github.com>
2024-08-28 17:10:04 +0530

    added two folders

---
+++ PreventDefault/app.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('#shelterForm');
+const input = document.querySelector('#catName');
+const list = document.querySelector('#cats');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(input.value);
+    const catName = input.value;
+    const newLi = document.createElement('LI');
+    newLi.innerText = catName;
+    console.log(newLi);
+    list.append(newLi);
+});
